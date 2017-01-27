4fbb2a01e06f4f0c286ad6a4e75a560ab7296406
Rob Brander <rob.brander@gmail.com>
2017-01-27 15:35:58 -0500

    Refactor calendar to use calendar-day

---
+++ src/components/calendar-day.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const CalendarDay = ({ date, isCompleted, onClick}) => {
+  const baseCSS = 'flex items-top justify-left pa1';
+  const today = new Date();
+  const isToday = today.toDateString() === date.toDateString();
+  const bgColour = (isToday ? 'bg-light-yellow' :'bg-white');
+  const completed = (isCompleted ? 'completed' : '');
+  const className = `day ${ baseCSS } ${ bgColour } ${ completed }`;
+  return (
+    <div className={ className } onClick={ onClick } >
+      { date.getDate() }
+    </div>
+  );
+}
+
+CalendarDay.propTypes = {
+  date: React.PropTypes.instanceOf(Date).isRequired,
+  isCompleted: React.PropTypes.bool.isRequired,
+  onClick: React.PropTypes.func,
+}
+
+export default CalendarDay;
--- src/components/calendar.js
+++ src/components/calendar.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { DECIMAL_RADIX } from '../constants';
+import CalendarDay from './calendar-day';
 import './calendar.css';
 
 const Calendar = ({ month, year, completedDays }) => {
   // make a date object for the first day of the given month/year
   const date = new Date(year, month - 1, 1);
-  const today = new Date();
-  const isTodayVisible = (
-    (today.getMonth() === date.getMonth()) &&
-    (today.getFullYear() === date.getFullYear())
-  );
+  const monthName = date.toDateString().split(' ')[1];
 
   // get the day of the week for the first day of the month
   const dayOfWeek = date.getDay(); // 0 = Sunday, 1 = Monday ... 6 = Saturday
@@ -26,19 +22,15 @@ const Calendar = ({ month, year, completedDays }) => {
   for (let week = 0; week < numWeeks; week++) {
     const row = [];
     for (let day = 0; day < DAYS_IN_WEEK; day++) {
-      const isFirstWeek = (week === 0);
       const date = (week * 7) + day - dayOfWeek + 1;
-      const isBlank = ((isFirstWeek && day < dayOfWeek) || (date > numberOfDaysInMonth));
-      const value = '  ' + (isBlank ? ' ' : date.toString());
-      row.push(value.substr(-2));
+      const isBlank = (date < 1) || (date > numberOfDaysInMonth)
+      row.push({
+        isBlank,
+        date: new Date(year, month - 1, date),
+      });
     }
     weeks.push(row);
   }
-
-  const monthName = date.toDateString().split(' ')[1];
-  // print the calendar in the console
-  // console.log('Calendar for ' + monthName +' ' + year);
-  // weeks.forEach(week => console.log(week.join(' ')));
 
   // Rendering
   // Calculate the percentage of the width each day will hold
@@ -50,39 +42,35 @@ const Calendar = ({ month, year, completedDays }) => {
         <div className="f3 tc">{monthName} {year}</div>
         <div className="flex flex-column bt br shadow-5" style={{width: '50vw'}}>
           { weeks.map((week, weekIdx) => {
-              // On the last week, we need to know the index of the first empty day
-              // for drawing borders
-              const firstBlankDayIdx = week.reduce((firstIdx, day, dayIdx) =>
-                (day === '  ') && dayIdx < firstIdx ? dayIdx : firstIdx, DAYS_IN_WEEK);
               return (
-                <div key={weekIdx + ':'} style={ {height: weekHeight} } className="bb flex">
+                <div
+                  key={ weekIdx + ':' }
+                  style={{ height: weekHeight }}
+                  className="bb flex"
+                >
                   { week.map((day, dayIdx) => {
-                      // blank days can appear in the first and last week
-                      // when there is a blank day, there shouldn't be a border applied
-                      const hasNoBorder = (
-                        ((weekIdx === 0) && (day === '  ') && (dayIdx > 0)) ||
-                        ((weekIdx === weeks.length - 1) && (dayIdx > firstBlankDayIdx))
-                      );
-                      const border = (hasNoBorder ? '' : ' bl');
-                      const isToday = (isTodayVisible && parseInt(day, DECIMAL_RADIX) === today.getDate());
-                      const bgColour = (day === '  ' ? ' bg-light-gray' :
-                        (isToday ? ' bg-light-yellow' :' bg-white')
-                      );
-                      const onClick = isToday ? (e) => {
-                        console.log('clicked');
-                      } : () => {};
-                      const completed = (isToday ? ' completed' : '');
+                      // Border left is needed on:
+                      // - all non-blank cells
+                      // - first cell of the calendar (if blank)
+                      // - last day + 1
+                      const isFirstCell = (weekIdx === 0) && (dayIdx === 0);
+                      const isDayAfterLast = (day.isBlank && dayIdx > 0 && !week[dayIdx - 1].isBlank);
+                      const border = (isFirstCell || isDayAfterLast || !day.isBlank) ? 'bl' : '';
+                      const background = (day.isBlank ? 'bg-light-gray' : 'bg-white');
                       return (
-                        <div  key={weekIdx + ':' + dayIdx}
-                              style={{width: `${pctWidth}%`}}
-                              onClick={onClick}
-                              className={
-                                'flex items-center justify-center' +
-                                bgColour +
-                                border +
-                                completed
-                              }
-                        >{ day }</div>
+                        <div
+                          key={weekIdx + ':' + dayIdx}
+                          style={{width: `${pctWidth}%`}}
+                          className={ border + ' ' + background }
+                        >{  day.isBlank ? null : (
+                              <CalendarDay
+                                date={ day.date }
+                                isCompleted={ false /* TODO */ }
+                                onClick={ () => {} }
+                              />
+                            )
+                          }
+                        </div>
                       );
                     })
                   }
